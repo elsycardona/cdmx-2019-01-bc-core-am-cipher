abe8514d6646876713a323c0a2e4f85d8044ebba
elsycardona <elsycp@gmail.com>
2019-01-14 15:23:58 -0600

    cambios en equipo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const root= documentElementById("root");
 const final= documentElementById("final");
+const offset= 3;
 
 function esconder(){ 
 root.classList.add('hideElement')
@@ -9,7 +10,19 @@ const ubicacion =
 document.getElementById ('ubicacion').value;
 console.log (ubicacion);
 
+codificar.addEventListener("click",msj);
+//Obteniendo el código ascii y convierte a mayusculas
+function msj (){
+    let mensajeObtenido =mensaje.value;
+    let mayuscula= mensajeObtenido.toUpperCase();
+    let mayus=mayuscula.charCodeAt();
 
-
+ for(let 1=0; i<mayuscula.length; i++){
+     let letraAscii= mayuscula.charCodeAt(i);
+     let formula= (letraAscii-65+offset)%26+65;
+     let resultado= String.fromCharCode(formula);
+     console.log(resultado);
+ }
+}
 }
 
