42aaefa9c39fd4007d9d05743442ad4c536610be
elsycardona <elsycp@gmail.com>
2019-01-14 12:26:32 -0600

    cambio lunes funcion

--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ const final= documentElementById("final");
 function esconder(){ 
 root.classList.add('hideElement')
 final.classList.remove('hideElement')
+
+const ubicacion =
+document.getElementById ('ubicacion').value;
+console.log (ubicacion);
+
+
+
 }
 
