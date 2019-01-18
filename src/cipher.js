83fb46b580907d5268a8bf4e8944590964b41089
elsycardona <elsycp@gmail.com>
2019-01-17 19:56:41 -0600

    test correcto

--- src/cipher.js
+++ src/cipher.js
@@ -1,38 +1,42 @@
 window.cipher = {
   // ... 
   encode: (offset, ubicacion) => {
-  
-    let mayuscula= ubicacion.toUpperCase();
-    let ubx= "";
 
- for(let i=0; i<mayuscula.length; i++){
-   let letraAscii= mayuscula.charCodeAt(i);
-   let formula= (letraAscii-65+offset)%26+65;
-   let resultado= String.fromCharCode(formula);
-     //console.log(resultado);
-   ubx+= resultado;
-    }
-  //console.log(ubx);
-  //alert(ubx);
-  ubxEscrito.innerHTML = ubx.toString();
+      let mayuscula = ubicacion.toUpperCase();
+      let ubx = "";
+
+      for (let i = 0; i < mayuscula.length; i++) {
+        let letraAscii = mayuscula.charCodeAt(i);
+        let formula = (letraAscii - 65 + offset) % 26 + 65;
+        let resultado = String.fromCharCode(formula);
+        //console.log(resultado);
+        ubx += resultado;
+      }
+      return ubx;
+
+      //console.log(ubx);
+      //alert(ubx);
+
     }
     //PINCHE COMA
     ,
 
 
-  decoding: (offsetD, ubicacionD) => {
+  decode: (offsetD, ubicacionD) => {
 
-    let mayusculaD= ubicacionD.toUpperCase();
-    let uby= "";
+    let mayusculaD = ubicacionD.toUpperCase();
+    let uby = "";
 
-  for(let i=0; i<mayusculaD.length; i++){
-    let letraAsciiD= mayusculaD.charCodeAt(i);
-    let formulaD= (letraAsciiD+65-offsetD)%26+65;
-    let resultadoD= String.fromCharCode(formulaD);
-    console.log(resultadoD);
-    uby+= resultadoD;
-     }
-  ubyEscrito.innerHTML = uby.toString();
-  }};
+    for (let i = 0; i < mayusculaD.length; i++) {
+      let letraAsciiD = mayusculaD.charCodeAt(i);
+      let formulaD = (letraAsciiD + 65 - offsetD) % 26 + 65;
+      let resultadoD = String.fromCharCode(formulaD);
+      //console.log(resultadoD);
+      uby += resultadoD;
+    }
+    return uby;
+    //ubyEscrito.innerHTML = uby.toString();
+  }
+};
 //JUEVES2
 //sifuncionaba
--- src/index.js
+++ src/index.js
@@ -1,56 +1,60 @@
-const root= document.getElementById("root");
-const code= document.getElementById("code");
-const decode= document.getElementById("decode");
-const pruebaCode= document.getElementById("pruebaCode");
-const pruebaDecodecode= document.getElementById("pruebaPagdecode");
-const returnCode= document.getElementById("returnCode")
-const returnDecode= document.getElementById("returnDecode")
+const root = document.getElementById("root");
+const code = document.getElementById("code");
+const codeD = document.getElementById("codeD");
+const pruebaCode = document.getElementById("pruebaCode");
+const pruebaDecode = document.getElementById("pruebaDecode");
+const returnCode = document.getElementById("returnCode");
+const returnDecode = document.getElementById("returnDecode");
+const ubxEscrito = document.getElementById("ubxEscrito");
+const ubyEscrito = document.getElementById("ubyEscrito");
 //...
 
-pruebaCode.addEventListener("click",()=>{
+pruebaCode.addEventListener("click", () => {
   root.classList.add('hideElement')
   code.classList.remove('hideElement')
-  const ubicacion= document.getElementById("ubicacion").value;
-  console.log(ubicacion)
-  const offset= parseInt(document.getElementById("offset").value);
-  console.log(offset)
+  const ubicacion = document.getElementById("ubicacion").value;
+  //console.log(ubicacion)
+  const offset = parseInt(document.getElementById("offset").value);
+  //console.log(offset)
   const ubxx = window.cipher.encode(offset, ubicacion);
-  ubicacion.innerHTML = ubxx;
+  ubxEscrito.innerHTML = ubxx
+
 })
 
-pruebaDecode.addEventListener("click",()=>{
+pruebaDecode.addEventListener("click", () => {
   root.classList.add('hideElement')
-  decode.classList.remove('hideElement')
-  const ubicacionD= document.getElementById("ubicacion").value;
-  console.log(ubicacionD)
-  const offsetD= parseInt(document.getElementById("offset").value);
-  console.log(offsetD)
-  const ubyy = window.cipher.decoding(offsetD, ubicacionD);
-  ubicacionD.innerHTML = ubyy;
+  codeD.classList.remove('hideElement')
+  const ubicacionD = document.getElementById("ubicacion").value;
+  //console.log(ubicacionD)
+  const offsetD = parseInt(document.getElementById("offset").value);
+  //console.log(offsetD)
+  const ubyy = window.cipher.decode(offsetD, ubicacionD);
+  ubyEscrito.innerHTML = ubyy;
+  //const ubyEscrito= document.getElementById("ubyEscrito");
 })
 
-returnCode.addEventListener("click",()=>{
+returnCode.addEventListener("click", () => {
   location.reload();
   code.classList.add('hideElement')
   root.classList.remove('hideElement')
 })
 
-returnDecode.addEventListener("click",()=>{
+returnDecode.addEventListener("click", () => {
   location.reload();
-  decode.classList.add('hideElement')
+  codeD.classList.add('hideElement')
   root.classList.remove('hideElement')
 })
 //COMENTARIO
 
 
 //function codificar (){
-  //var caracteres = [];
-  //valor = ubicacion.value;
-  //for (var i=0; i<valor.length; i++){
-    //caracteres[i] = valor.charAt(i).charCodeAt(0);
-    //ascii const = i => i.charCodeAt (0);
-  //}
-    //code.innerHTML = caracteres.toString();
+//var caracteres = [];
+//valor = ubicacion.value;
+//for (var i=0; i<valor.length; i++){
+//caracteres[i] = valor.charAt(i).charCodeAt(0);
+//ascii const = i => i.charCodeAt (0);
+//}
+//code.innerHTML = caracteres.toString();
 //}
 
 
@@ -61,16 +65,15 @@ returnDecode.addEventListener("click",()=>{
 //codificar.addEventListener("click",msj);
 //Obteniendo el código ascii y convierte a mayusculas
 //function msj (){
-  //  let mensajeObtenido =mensaje.value;
-   // let mayuscula= mensajeObtenido.toUpperCase();
-    //let mayus=mayuscula.charCodeAt();
+//  let mensajeObtenido =mensaje.value;
+// let mayuscula= mensajeObtenido.toUpperCase();
+//let mayus=mayuscula.charCodeAt();
 
- //for(let 1=0; i<mayuscula.length; i++){
-   //  let letraAscii= mayuscula.charCodeAt(i);
-     //let formula= (letraAscii-65+offset)%26+65;
-     //let resultado= String.fromCharCode(formula);
-     //console.log(resultado);
- //}
+//for(let 1=0; i<mayuscula.length; i++){
+//  let letraAscii= mayuscula.charCodeAt(i);
+//let formula= (letraAscii-65+offset)%26+65;
+//let resultado= String.fromCharCode(formula);
+//console.log(resultado);
 //}
 //}
-
+//}
